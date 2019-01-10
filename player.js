3e9a33b643539d6178b11905df53d4c7573444a1
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2019-01-10 15:34:17 +0100

    02-scope-module

--- player.js
+++ player.js
@@ -1,41 +1,52 @@
-function init() {
-  max = 600;
-  media = document.getElementById('media');
-  play = document.getElementById('play');
-  bar = document.getElementById('bar');
-  progress = document.getElementById('progress');
-  play.addEventListener('click', click, false);
-  bar.addEventListener('click', move, false);
-}
-function click() {
-  if (!media.paused && !media.ended) {
-    media.pause();
-    play.innerHTML = 'Play';
-    window.clearInterval(loop);
-  } else {
-    media.play();
-    play.innerHTML = 'Pause';
-    loop = setInterval(state, 1000);
+const player = (function(view) {
+  const max = 600;
+  let GUI = {};
+  let loop;
+  function init() {
+    GUI = {
+      media: view.getElementById('media'),
+      play: view.getElementById('play'),
+      bar: view.getElementById('bar'),
+      progress: view.getElementById('progress')
+    };
+    GUI.play.addEventListener('click', click, false);
+    GUI.bar.addEventListener('click', move, false);
   }
-}
-
-function state() {
-  if (!media.ended) {
-    var total = parseInt((media.currentTime * max) / media.duration);
-    progress.style.width = total + 'px';
-  } else {
-    progress.style.width = '0px';
-    play.innerHTML = 'Play';
-    window.clearInterval(loop);
+  function click() {
+    if (!GUI.media.paused && !GUI.media.ended) {
+      GUI.media.pause();
+      GUI.play.innerHTML = 'Play';
+      clearInterval(loop);
+    } else {
+      GUI.media.play();
+      GUI.play.innerHTML = 'Pause';
+      loop = setInterval(state, 1000);
+    }
   }
-}
-
-function move(e) {
-  if (!media.paused && !media.ended) {
-    var mouseX = e.pageX - bar.offsetLeft;
-    var newTime = (mouseX * media.duration) / max;
-    media.currentTime = newTime;
-    progress.style.width = mouseX + 'px';
+  function move(e) {
+    if (!GUI.media.paused && !GUI.media.ended) {
+      const mouseX = e.pageX - bar.offsetLeft;
+      const newTime = (mouseX * media.duration) / max;
+      GUI.media.currentTime = newTime;
+      progress.style.width = mouseX + 'px';
+    }
   }
-}
-window.addEventListener('load', init, false);
+  function state() {
+    if (!GUI.media.ended) {
+      const total = parseInt(
+        (GUI.media.currentTime * max) / GUI.media.duration
+      );
+      GUI.progress.style.width = total + 'px';
+    } else {
+      GUI.progress.style.width = '0px';
+      GUI.play.innerHTML = 'Play';
+      clearInterval(loop);
+    }
+  }
+  return {
+    init,
+    click,
+    move
+  };
+})(window.document);
+window.addEventListener('load', player.init, false);
